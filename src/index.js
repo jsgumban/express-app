8da654c0eb84aa47b803f27c6fec8c1122dd6ade
John Hel Gumban <gumban.johnhel@gmail.com>
2025-07-23 16:38:22 +0800

    Update test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-// src/index.js
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello, Docker + GitHub Actions!')
-})
+	res.send('Hello, Docker + GitHub Actions!');
+});
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-	console.log(`Server running at http://localhost:${PORT}`)
-})
+if (require.main === module) {
+	const PORT = process.env.PORT || 3000;
+	app.listen(PORT, () => {
+		console.log(`Server running at http://localhost:${PORT}`);
+	});
+}
+
+module.exports = app;
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,6 +1,6 @@
+// test/main.test.js
 const request = require('supertest');
-const app = 'http://localhost:3000'; // Replace with your app's URL
-
+const app = require('../src/index');
 
 describe('GET /', () => {
 	it('should return a 200 status code and a greeting message', async () => {
@@ -8,4 +8,4 @@ describe('GET /', () => {
 		expect(response.statusCode).toBe(200);
 		expect(response.text).toBe('Hello, Docker + GitHub Actions!');
 	});
-})
+});
